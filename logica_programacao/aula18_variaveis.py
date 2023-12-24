e433f08e498faa072fa1f7dccfc39e1422ae3d95
Emily Crystie <emily_crystie@hotmail.com>
2023-12-24 08:34:10 -0300

    atualização conteudo

--- logica_programacao/aula18_variaveis.py
+++ logica_programacao/aula18_variaveis.py
@@ -6,3 +6,16 @@ peso = 82
 imc = int(peso /altura ** 2)
 
 print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+
+# identificação do elemento na memória
+# mesmo id
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+
+# ids diferentes
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
